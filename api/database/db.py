4455d9cb4a5f36406d88bf1ca1b0713e8adcffa0
Klayton <klaki892@gmail.com>
2019-12-14 20:35:28 -0500

    configure herokudb connection

--- api/database/db.py
+++ api/database/db.py
@@ -1,11 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from flask import current_app as app
 
 def getSession( user, passwd):
+
+    if not os.getenv("DATABASE_URL"):
+        raise RuntimeError("DATABASE_URL is not set")
+
     Session = sessionmaker(autocommit=False,
                            autoflush=False,
-                           bind=create_engine('mysql+pymysql://{}:{}@localhost/app'.format(user, passwd))
+                           bind=create_engine(os.getenv("DATABASE_URL"))
                           )
     return scoped_session(Session)
